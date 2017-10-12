aed2bda13509abdee4c31c45c5703dc025158e58
Jacob O'Donnell <jacobodonnell@gmail.com>
2017-10-12 08:04:38 -0400

    moving things out of packs because apparently thats for entry points only

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import DrillPage from './drill_page'
+import DrillPage from '../drill/drill_page'
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.body.appendChild(document.createElement('div'))
